e64daec1cd4f5f222c1f86b4c734e4aba302c898
alvaroaleman14 <alvaro14uma@uma.es>
2021-11-25 17:36:23 +0100

    Añadidos ejercicios en la relación 2

--- Relaciones de Ejercicios/Relacion 2/relacion2.hs
+++ Relaciones de Ejercicios/Relacion 2/relacion2.hs
@@ -68,7 +68,7 @@ replicate' 0 n = [ ]
 replicate' 1 n = [n]
 replicate' x n = n : replicate' (x-1) n
 
--- b)
+    -- b)
 p_replicate' n x = n >= 0 && n <= 1000 ==> length (filter (==x) xs) == n
                                            && length (filter (/=x) xs) == 0
    where xs = replicate' n x
@@ -80,6 +80,62 @@ divisores x = [n | n <- [1..x], divideA n x]
      divideA n x = if mod x n == 0 then True else False
 
 divisores' :: Int -> [Int]
-divisores' x = [n | n <- [-x..(-1)], divideA n x] ++ divisores x
+divisores' x | x < 0 =  ([n | n <- [x..(-1)], divideA n x] ++ divisores (-x))
+             | otherwise = divisores x
  where
      divideA n x = if mod x n == 0 then True else False
+
+-- Ejercicio 7 
+    --a)
+mcd :: Int -> Int -> Int 
+mcd x y | x==0 || y==0 = error "No hay maximo comun divisor si uno es nulo"
+        | otherwise =  maximum(intersect (divisores' x) (divisores' y))
+
+    -- c)
+mcm :: Int -> Int -> Int 
+mcm x y | x==0 || y==0 = error "No hay mcm si uno es nulo"
+        | otherwise =  div (x*y) (mcd x y)
+
+-- Ejercicio 8 
+ -- Partiendo de que 1 no es primo. Un num es primo si tiene como divisores 1 y p
+    -- a)
+esPrimo :: Int -> Bool 
+esPrimo x | length (divisores x) == 2 = True
+          | otherwise = False 
+    -- b)
+primosHasta :: Int -> [Int]
+primosHasta x = [n | n <- [1..x], esPrimo n == True ]
+
+    -- c)
+primosHasta' :: Int -> [Int]
+primosHasta' x = filter (esPrimo) [2..x]
+
+-- Ejercicio 9 
+ -- Conjetura de Goldbach: "Todo num par > 2 puede escribirse como la suma de dos primos"
+pares :: Int -> [(Int,Int)]
+pares x | mod x 2 /= 0 = [ ]
+        | otherwise = [(n,y) | n <- primosHasta (div x 2), y <- primosHasta x, (n + y) == x]
+ -- b)
+goldbach :: Int -> Bool 
+goldbach x = if length (pares x) >= 1 then True else False
+
+-- Ejercicio 10
+ -- a) 
+-- Redefino la funciones divisores, pero quitandole el ultimo valor
+
+esPerfecto :: Int -> Bool 
+esPerfecto x = sum (divisores x) - x == x
+
+ -- b)
+perfectosMenoresQue :: Int -> [Int]
+perfectosMenoresQue x = filter (esPerfecto) [1..x]
+
+-- Ejercicio 11
+ -- a) 
+take' :: Int -> [a] -> [a]
+take' n xs = [ x | (p,x) <- zip [0..(n-1)] xs]
+ 
+ --b)
+drop' :: Int -> [a] -> [a]
+drop' n xs | n >= length xs = [ ]
+           | otherwise = [ x | (p,x) <- zip [1..(length  xs)] xs, p>n]
